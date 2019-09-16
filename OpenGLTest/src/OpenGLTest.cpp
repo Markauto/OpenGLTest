fb743968cd448cd9fd09232f12930148529db718
Mark Peart <markpeart189@outlook.com>
2019-09-16 19:14:08 +0100

    Index Buffer

--- OpenGLTest/src/OpenGLTest.cpp
+++ OpenGLTest/src/OpenGLTest.cpp
@@ -122,19 +122,30 @@ int main(void)
 
     TheLogger->Log(helpers::StringFormater::Format("OpenGL Version: %s", (const char*)glGetString(GL_VERSION)));
     
-    float positions[6] = {
+    float positions[] = {
          -0.5f, -0.5f,
-        - 0.0f,  0.5f,
-          0.5f, -0.5f
+          0.5f, -0.5f,
+          0.5f,  0.5f,
+         -0.5f,  0.5f
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
     };
 
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6, positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float) , positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, false, sizeof(float) * 2, 0);   
+
+    unsigned int indexBufferObject;
+    glGenBuffers(1, &indexBufferObject);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObject);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     auto [vertexShaderSource, fragmentShaderSource] = ParseShader("res/shaders/Basic.glsl");    
     unsigned int shader = CreateShader(vertexShaderSource, fragmentShaderSource);
@@ -144,7 +155,7 @@ int main(void)
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
         
         glfwSwapBuffers(window);
 
